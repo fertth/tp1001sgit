56f8bece19f31e926ad94cabdb7fc2cae0805bb5
julietth <julietthcontreras1412@hotmail.com>
2020-10-27 18:04:21 -0700

    hola

--- shapes1.py
+++ shapes1.py
@@ -1,14 +1,40 @@
 import turtle
 from turtle import *
 from turtle import Screen
-from freegames import vector
     
 def emptySquare():
-    pass  # TODO
+      t = turtle.Turtle()
+      for i in range(4):
+         t.forward(50)
+         t.right(90)  
+
     
 def filledSquare():
-  pass  # TODO
-  
+    s = int(input("Enter the length of the side of the square: ")) 
+    col = input("Enter the color name or hex value of color(# RRGGBB): ")
+    t = turtle.Turtle()
+    t.fillcolor(col)
+    t.begin_fill()
+    for i in range(4):
+        t.forward(s)
+        t.right(90)
+    # Rotate clockwise by 90 degrees
+    t.end_fill()
+
+
+
+def emptyCircle():
+    t = turtle.Turtle()
+    t.circle(100)
+
+def filledCircle():
+    t = turtle.Turtle()
+    t.fillcolor('red')
+    t.begin_fill()
+    t.circle(100)
+    t.end_fill()
+
+
 while True:
     screen = Screen()
     answer = screen.textinput("Next Game","1 - Square:")
@@ -18,4 +44,7 @@ while True:
         emptySquare()
     elif (answer == '2'):
         filledSquare()
- 
+    elif (answer == '3'):
+        emptyCircle()
+    elif (answer == '4'):
+        filledCircle()
